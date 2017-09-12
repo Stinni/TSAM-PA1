04429a81b64dbac700dc018a6d5c4d348722295a
Stinni <kristinnf13@ru.is>
2017-09-12 01:14:29 +0000

    security/folder stuffs ready

--- src/tftpd.c
+++ src/tftpd.c
@@ -6,7 +6,7 @@
 // Constants:
 #define MAX_MESSAGE_LENGTH   512
 #define MAX_PACKET_SIZE      516
-#define MAX_FILE_PATH_LENGTH 255
+#define MAX_FILE_NAME_LENGTH 255
 #define MAX_TIMEOUTS           5
 
 // OpCodes defined for readability
@@ -36,38 +36,112 @@
 #include <arpa/inet.h>
 //#include <sys/select.h>
 //#include <time.h>
-//#include <errno.h>
+#include <errno.h>
 #include <string.h>
 #include <ctype.h>
+#include <limits.h> /* PATH_MAX */
 
-//void RRequest(int sockfd, struct client, int message)
-void RRequest()
+void RRequest(int sockfd, struct sockaddr_in client, char *msg, char *folder_path, char *base_path)
 {
-  //message[0] + [1] = opcode
-  //messagge[2] = filename
 
+	char file_name[MAX_FILE_NAME_LENGTH], tmp[PATH_MAX];
+	char *file_path;
+
+	strcpy(tmp, folder_path);
+	strcpy(file_name, &msg[2]);
+	strcat(tmp, file_name);
+
+	file_path = realpath(tmp, NULL);
+
+	if(file_path == NULL) {
+		printf("HERE WE NEED TO SEND AN ERROR PACKET BECAUSE REALPATH COULDN'T BE RESOLVED!\n");
+	}
+	else {
+		if(strncmp(base_path, file_path, strlen(base_path)) != 0)
+		{
+			printf("HERE WE NEED TO SEND AN ERROR PACKET BECAUSE THE FILE ISN'T IN THE ALLOWED FOLDER!\n");
+		}
+		else {
+			printf("WOOHOO!!!\n");
+		}
+	}
+
+	//fp = fopen(file_path, "r");
 }
 
 void WRequest(int sockfd, struct sockaddr_in client)
 {
-	printf("WRQ request not allowed on server! \n");
+	printf("WRQ request not allowed on server!\n");
 	char errorPacket[45];
 	errorPacket[0] = 0; errorPacket[1] = OP_ERR; errorPacket[2] = 0; errorPacket[3] = ERR_IOP;
 	strcpy(&errorPacket[4], "Uploading is not allowed on this server!");
 	sendto(sockfd, errorPacket, sizeof(errorPacket), 0, (struct sockaddr *) &client, (socklen_t) sizeof(client));
 }
 
+/*void ErrorReceived(struct sockaddr_in client, char message)
+{
+	// hér væri mögulega hægt að prenta bara út errorkóðann og errorskilaboðið í message
+	// viljum samt segja eitthvað eins og "Client 127.0.0.1:2000 reported error:"
+	printf("Received error message from %s", client);
+	char errcode = message[4];
+	switch(errcode)
+	{
+		case ERR_NOT :
+			printf("Not defined, see error message (if any).");
+			break;
+		case ERR_FILE :
+			printf("File not found.");
+			break;
+		case ERR_ACC :
+			printf("Access violation.");
+			break;
+		case ERR_DISK :
+			printf("Disk full or allocation exceeded.");
+			break;
+		case ERR_IOP :
+			printf("Illegal TFTP operation.");
+			break;
+		case ERR_UID :
+			printf("Unknown transfer ID.");
+			break;
+		case ERR_FEX :
+			printf("File already exists.");
+			break;
+		case ERR_USR :
+			printf("No such user.");
+			break;
+	}
+}*/
+
 int main(int argc, char *argv[])
 {
+
 	int socket_addr;
+	char *folder_path, *base_path;
 
 	if(argc >= 3) {
 		socket_addr = atoi(argv[1]);
+
+		int tmp = strlen(argv[2]) + 2;
+		folder_path = malloc(tmp);
+		strcpy(folder_path, argv[2]);
+		folder_path[tmp-2] = '/';
+		folder_path[tmp-1] = '\0';
+		base_path = realpath(folder_path, NULL);
+
+		if(base_path == NULL) {
+			free(folder_path);
+			printf("Invalid directory provided!\n");
+			exit(EXIT_FAILURE);
+		}
 	}
 	else {
 		printf("Format expected is .src/tftp <port_number> <directory>\n");
-		exit(EXIT_SUCCESS);
+		exit(EXIT_FAILURE);
 	}
+
+	printf("folder_path: %s\n", folder_path);
+	printf("base_path: %s\n", base_path);
 
 	int sockfd;
 	struct sockaddr_in server, client;
@@ -106,14 +180,10 @@ int main(int argc, char *argv[])
 
 		if (n >= 0) {
 			message[n] = '\0';
-			//printf("Received:\n");
-			//for(unsigned int i = 0; i < n; i++) printf("%hhx ", message[i]);
-			//printf("\n");
-			//fflush(stdout);
 			char opcode = message[1];
 			switch(opcode) {
 				case OP_RRQ :
-					//RRequest(sockfd, client, message);
+					RRequest(sockfd, client, message, folder_path, base_path);
 					break;
 				case OP_WRQ :
 					WRequest(sockfd, client);
@@ -122,15 +192,25 @@ int main(int argc, char *argv[])
 					printf("%s sent a data packet but it is not supported\n", (char *)inet_ntoa(client.sin_addr));
 					break;
 				case OP_ACK :
-					printf("Acknowledgment \n");
+					printf("Acknowledgment\n");
 					break;
 				case OP_ERR :
-					printf("Error \n");
+					printf("Error\n");
+					//ErrorReceived(client, message);
 					break;
+				default:
+					printf("Opcode unrecognised!\n");
 			}
-		} else {
-			// Error or timeout. Check errno == EAGAIN or
-			// errno == EWOULDBLOCK to check whether a timeout happened
+			fflush(stdout);
+		}
+		else {
+			if(errno == EAGAIN || errno == EWOULDBLOCK) {
+				printf("Timeout happened...\n");
+			}
+			else {
+				printf("Error happened...\n");
+			}
+			fflush(stdout);
 		}
 	}
 }
